809d5e188f44a9274e7381a6a33d19570e4d25ab
Niranjan Bekoju <bekoju.niranjan@gmail.com>
2019-01-25 22:12:45 +0545

    HomePage User Interface Design

---
+++ PersonalityPrediction/src/personalityprediction/PersonalityPrediction.java
@@ -0,0 +1,14 @@
+package personalityprediction;
+
+/**
+ * @Author: Niranjan Bekoju
+ * @Address: Liwali Bhaktapur
+ * @College: Khwopa College of Engineering
+ */
+public class PersonalityPrediction {
+
+    public static void main(String[] args) {
+        HomePage h = new HomePage();
+        h.setVisible(true);
+    }
+}
